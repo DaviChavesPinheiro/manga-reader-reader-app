dc94e10b525f8ac5548fb6000a579a1127cb3cc0
Davi <davicpinheiro123@hotmail.com>
2020-09-12 23:35:04 -0300

    HashHistory

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import App from './App'
 
@@ -13,9 +13,9 @@ import store from "./store/store";
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <App></App>
-    </BrowserRouter>
+    </HashRouter>
   </Provider>,
   document.getElementById('root')
 );
