dd9dd6e0a7bdc3ee828d558aa82249a0776a4051
Davi <davicpinheiro123@hotmail.com>
2020-09-13 21:40:00 -0300

    MangaPage Style Changed

--- src/components/manga/MangaProfile.jsx
+++ src/components/manga/MangaProfile.jsx
@@ -3,34 +3,51 @@ import { Link } from "react-router-dom";
 import "./MangaProfile.css";
 
 import If from "../../operator/If";
+import IconButton from '../IconButton'
+
 
 const MangaProfile = props => {
     const manga = props.manga
     return (
         <div className="manga-profile">
-            <h2>{manga.title}</h2>
-            <div className="header">
-                <img src={manga.image_url} alt="Manga" />
-                <div className="info">
-                    <p><strong>Chapters:</strong> {manga.chapters_amount}</p>
-                    <p><strong>Score:</strong> {manga.score}</p>
-                    <p><strong>Members:</strong> {manga.members}</p>
+
+            <section className="one">
+                <div className="banner-container">
+                    <img src={manga.image_url} alt="Manga" />
                 </div>
-            </div>
-            <div className="body">
+                <div className="main-info">
+                    <div className="img-container">
+                        <img src={manga.image_url} alt="Manga" />
+                    </div>
+                    <h1>{manga.title}</h1>
+                    <span>{manga.score}</span>
+                </div>
+                <div className="buttons-container">
+                    <IconButton icon="play" label="Resume"></IconButton>
+                    <IconButton icon="navicon" label={`${manga.chapters_amount} Chapters`}></IconButton>
+                    <IconButton icon="heart-o" label="Favorite"></IconButton>
+                </div>
+            </section>
+            <section className="two">
                 <If test={manga.description !== undefined}>
                     <div className="description">
                         <p>Description: {manga.description}</p>
                     </div>
                 </If>
-                <div className="episodes">
+
+            </section>
+            <section className="three">
+            <If test={manga.chapters !== undefined}>
+                <div className="chapters-list">
+                    <h2>Chapters</h2>
                     <ul>
                         {manga.chapters ? manga.chapters.map((chapter, index) => (
                             <Link key={index} to={`/manga/${manga._id}/chapters/${index}`}><li>{chapter.title}</li></Link>
                         )) : null}
                     </ul>
                 </div>
-            </div>
+            </If>
+            </section>
         </div>
     )
 }
