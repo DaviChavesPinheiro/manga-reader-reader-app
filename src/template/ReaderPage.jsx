f080eec233a301f779638c422cb8653c282c9619
Davi <davicpinheiro123@hotmail.com>
2020-09-16 18:20:17 -0300

    Infinity Scrolling

--- src/template/ReaderPage.jsx
+++ src/template/ReaderPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useParams } from "react-router-dom";
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
@@ -9,12 +9,15 @@ import axios from "axios";
 import "./ReaderPage.css";
 
 import LazyLoad from 'react-lazyload';
+import If from "../operator/If";
 
 
 const ReaderPage = props => {
     const { idManga, idChapter } = useParams()
+    const [chapters, setChapters] = useState([])
     const [pages, setPages] = useState([])
     const [chapterIndex, setChapterIndex] = useState(parseInt(idChapter))
+    const nextChapterRef = useRef()
 
 
     useEffect(() => {
@@ -22,22 +25,24 @@ const ReaderPage = props => {
         props.setHideOnScrool(true)
 
         loadChapter(chapterIndex)
-    }, [])
 
-    function loadChapter(index) {
-        axios.get(`https://charlotte-services.herokuapp.com/mangas/${idManga}/chapters/${index}`).then(res => {
-            const chapter = res.data.chapters[0]
-            if (chapter && chapter.pages) {
-                setPages(chapter.pages)
-            }
-            props.setDisplayLabel(`${res.data.title} - ${chapter ? chapter.title : ''}`)
-            props.selectManga({ ...res.data, chapters: [] })
-        })
-    }
+        let observer = new IntersectionObserver(onArriveOnEnd, {
+            threshold: 0.25
+        });
+        // observer.observe(nextChapterRef.current)
 
-    function goToChapter(index) {
+        if (window.history.scrollRestoration) {
+            window.history.scrollRestoration = 'manual';
+        }
         document.body.scrollTop = 0;
         document.documentElement.scrollTop = 0;
+
+    }, [])
+
+
+    function goToChapter(index) {
+        // document.body.scrollTop = 0;
+        // document.documentElement.scrollTop = 0;
 
         window.history.pushState('', '', `/#/manga/${idManga}/chapters/${index}`);
 
@@ -46,19 +51,44 @@ const ReaderPage = props => {
         setChapterIndex(index)
     }
 
+    function loadChapter(index) {
+        axios.get(`https://charlotte-services.herokuapp.com/mangas/${idManga}/chapters/${index}`).then(res => {
+            const chapter = res.data.chapters[0]
+            if (chapter && chapter.pages) {
+                setPages(chapter.pages)
+                setChapters([...chapters, {index: index, pages: chapter.pages}])
+            }
+            props.setDisplayLabel(`${res.data.title} - ${chapter ? chapter.title : ''}`)
+            props.selectManga({ ...res.data, chapters: [] })
+        })
+    }
+
+    function onArriveOnEnd(entries, observer) {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) return
+            console.log(entry)
+
+            observer.unobserve(entry.target)
+        });
+    }
+
     return (
         <div className="reader-page">
-            <div className="reader-pages-container">
-                {pages.map((page) => (
-                    <LazyLoad key={page} height={900}>
-                        <img src={page}></img>
-                    </LazyLoad>
-                ))}
-                <div className="next-chapter-area">
-                    <h3>End Of Chapter {chapterIndex + 1}</h3>
-                    <button onClick={() => goToChapter(chapterIndex + 1)}>Load Next Chapter</button>
+            {chapters.map((chapter) => (
+                <div className="reader-pages-container" key={chapter.index} id={`chapter-${chapter.index}`}>
+                    {chapter.pages.map((page) => (
+                        <LazyLoad key={page} height={900}>
+                            <img src={page}></img>
+                        </LazyLoad>
+                    ))}
+                    <div className="next-chapter-area" ref={nextChapterRef}>
+                        <h3>End Of Chapter №{chapter.index + 1}</h3>
+                        <If test={chapters[chapter.index + 1] === undefined}>
+                            <button onClick={() => goToChapter(chapter.index + 1)}>Load Next Chapter</button>
+                        </If>
+                    </div>
                 </div>
-            </div>
+            ))}
         </div>
     )
 }
